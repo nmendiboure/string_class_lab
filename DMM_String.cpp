a5ba82596bb189e46678cd15d1ec11b77d0a177c
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2020-04-02 12:10:45 +0200

    Version de DMM du .cpp

---
+++ DMM_String.cpp
@@ -0,0 +1,96 @@
+#include "OurString.h"
+
+// Student A
+OurString::OurString(OurString &string)
+{
+
+};
+
+char* OurString::c_str()
+{
+
+};
+
+size_t OurString::size()
+{
+
+};
+
+void OurString::clear()
+{
+
+};
+
+OurString OurString::operator=(char c)
+{
+
+};
+
+OurString OurString::operator+(const string&, const char* s)
+{
+
+};
+
+
+// Student B
+OurString::OurString(char &string)
+{
+
+};
+
+size_t OurString::length()
+{
+
+};
+
+size_t OurString::max_size()
+{
+
+};
+
+void OurString::resize(size_t n, char c)
+{
+
+};
+
+OurString OurString::operator=(const string&)
+{
+
+};
+
+OurString OurString::operator+(const string&, char c)
+{
+
+};
+
+
+// Student C
+OurString::~OurString()
+{
+
+};
+
+size_t OurString::capacity()
+{
+
+};
+
+bool OurString::empty()
+{
+
+};
+
+void OurString::reserve(size_t n = 0)
+{
+
+};
+
+OurString OurString::operator=(const char* s)
+{
+
+};
+
+OurString OurString::operator+(const string&, const string&)
+{
+
+};
