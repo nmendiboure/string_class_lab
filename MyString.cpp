e730956952c2c312c8563899aeeb196ab4ee34f8
Charles Nodot <charles.nodot@insa-lyon.fr>
2020-04-06 15:03:51 +0200

    "Ajout de MyString.cpp"

---
+++ MyString.cpp
@@ -0,0 +1,117 @@
+#include "MyString.h"
+
+//Student A
+MyString::MyString(MyString &string){ //constructeur par défaut
+
+
+
+
+};
+
+
+char* MyString::c_str(){
+
+};
+
+
+size_t MyString::size(){
+
+
+};
+
+
+
+void MyString::clear(){
+
+
+};
+
+
+MyString MyString::operator=(char c){
+
+
+
+};
+
+MyString MyString::operator+(const string&,const char* s){
+
+
+};
+
+
+
+//Student B
+
+MyString::MyString(char &string){
+
+
+};
+
+
+
+size_t MyString::length(){
+
+
+};
+
+
+
+size_t MyString::max_size(){
+
+
+};
+
+
+
+void MyString::resize(size_t n,char c){
+
+
+};
+
+
+MyString MyString::operator=(const string&){
+
+
+
+};
+
+
+
+MyString MyString::operator+(const string&, char c){
+
+
+};
+
+
+
+//Student C
+
+
+
+MyString::~MyString(){
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
