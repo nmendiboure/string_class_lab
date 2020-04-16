c907f291fff770a03c6121bc539f7e3626bb17d3
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2020-04-16 14:56:03 +0200

    Test de notre classe MyString avce le gtest

--- MyString_test.cpp
+++ MyString_test.cpp
@@ -3,18 +3,18 @@
 #include "MyString.cpp"
 TEST(GTestTests, DummyTest) {
 	MyString string;
-	bool check = true;
+	bool test = true;
 	char* c_string = string.c_str();
 	char hello[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r','l', 'd', '\0'};
 	short int i = 0;
 	while(c_string[i] != '\0' && hello[i] != '\0'){
 		if (c_string[i] != hello[i])
 		{
-			check = false;
+			test = false;
 		}
 		i++;
 	}
-	EXPECT_TRUE(check);
+	EXPECT_TRUE(test);
 }
 
 
