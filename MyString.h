c2b56d784bcd17b20ce671ff5870d6df55903212
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2020-04-19 14:54:42 +0200

    Changement de nom des variables en paramètre pour plus de coherance avec le .cpp

--- MyString.h
+++ MyString.h
@@ -1,22 +1,23 @@
 class MyString {
 	
 	protected : 
-		char* string_; //pointeur vers notre le caracter alloué
-		size_t len_(const char* s) const ; 
+		char* string_; // Pointer to the characters contained in the String
+		size_t length_; // Number of characters allocated in data
 
 	public :
 		//Student A : copy - constructor
 		MyString (const MyString &string);
 		char * c_str() const;
 		void clear();
-		size_t size() const; //size_t is an unsigned integral type (the same as member type string::size_type).
+		size_t size() const; 
 		MyString operator=(char c);
-		MyString operator+(const char* string);
+		MyString operator+(const char* str);
 
 		//Student B : constructor from a c-string
-		MyString(); 	//Default constructor
-		MyString(const char* c); //MyString constructor
-		size_t length() const;
+		MyString(); 	
+		MyString(const char* str); 
+		size_t length() const; 
+		size_t len_(const char* str) const ;
 		size_t max_size() const;
 		void resize(size_t n, char c);
 		MyString operator=(const MyString &string);
