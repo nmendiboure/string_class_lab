3e8483d9ef18ee85baae1dbfb2ea160e4dabab1b
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2020-04-19 14:52:56 +0200

    Ajout des opérateurs + et = pour le Student B AJout de commentaires Optimisations mineures des méthodes

--- MyString.cpp
+++ MyString.cpp
@@ -2,19 +2,23 @@
 
 // ###################   Student A  ######################
 
-MyString::MyString(const MyString &string){  //copy constructor
+MyString::MyString(const MyString &string){  
+/*copy constructor, takes a MyString Object and return a new Object with the same content*/
 
 	const char* Cstring = string.c_str();
-	size_t length_ = len_(Cstring) + 1;
+	length_ = len_(Cstring) + 1;
 	string_ = new char[length_];
 	memcpy(string_, Cstring, length_);
 };
 
-char* MyString::c_str() const { // getter which return an array of char 
+char* MyString::c_str() const { 
+/* getter which return an array of characters */
 	return string_;
 };
 
+
 size_t MyString::size() const{
+/*Return the size of the content, same as the length() method*/
 	size_t i = 0;
 	while(string_[i]!='\0'){
 		i++;
@@ -25,7 +29,10 @@ size_t MyString::size() const{
 
 
 void MyString::clear(){
-// void * memset ( void * ptr, int value, size_t num  -> Fill block of memory;
+/* 
+Clear the content of a MyString Object, 
+void * memset ( void * ptr, int value, size_t num  -> Fill block of memory; */
+
 	memset (string_, 0, MyString::size() + 1);
 };
 
@@ -34,14 +41,15 @@ void MyString::clear(){
 	;
 };
 
-MyString MyString::operator+(const char* string){
+MyString MyString::operator+(const char* str){
 	;
 };*/
 
 
 // ############### STUDENT B ##################
 
-MyString::MyString(){ //constructeur par défaut
+MyString::MyString(){ 
+/* Default Constructor which creates a array of chars "Hello World"*/ 
 
 	char string[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r','l', 'd', '\0'};
 	size_t i = 0;
@@ -55,13 +63,14 @@ MyString::MyString(){ //constructeur par défaut
 
 
 
-MyString::MyString(const char* c){
+MyString::MyString(const char* str){
+/*Constructor that initializes a MyString object from a c-string passed as argument */
 
-	 if ((c != NULL) && (c[0] != '\0')){
-		size_t n = len_(c);
+	 if ((str != NULL) && (str[0] != '\0')){
+		size_t n = len_(str);
 		string_ = new char[n+1];
 		for (unsigned i=0; i < n+1; i++){
-			string_[i] = c[i];
+			string_[i] = str[i];
 		}
 		string_[n+1]='\0';
 	}	
@@ -73,17 +82,18 @@ MyString::MyString(const char* c){
 
 
 size_t MyString::max_size() const {
+/* We fixed the maximun length of the content to 100*/
 	return 100;
 };
 
 
-size_t MyString::len_(const char* c) const{
+size_t MyString::len_(const char* str) const{
+/*return the size for a c_string*/
 	int i =0;
-	while (c[i] != '\0'){
+	while (str[i] != '\0'){
 		i++;
 	}
 	return i;
-
 };
 
 size_t MyString::length() const{
@@ -99,6 +109,16 @@ size_t MyString::length() const{
 
 
 void MyString::resize(size_t n,char c){
+/*
+Resizes the string to a length of n characters.
+
+If n is smaller than the current string length, the current value is shortened to its first n character, removing the characters beyond the nth.
+
+If n is greater than the current string length, the current content is extended by inserting at the end as many characters as needed to reach a size of n. If c is specified, the new elements are initialized as copies of c, otherwise, they are value-initialized characters (null characters).
+
+see more on http://www.cplusplus.com/reference/string/string/resize/
+*/
+
 	if (n <= MyString::max_size()){
 		if (MyString::length() < n){
 
@@ -129,15 +149,37 @@ void MyString::resize(size_t n,char c){
 
 
 
-/*MyString MyString::operator=(const MyString &string){
-	;
+MyString MyString::operator=(const MyString &string){
+	if(this == &string){
+		return *this;
+	}
+	else {
+		length_ = len_(string.c_str()+1);
+		string_  = new char[length_];
+		for (unsigned i=0; i < length_; i++){
+		   	string_[i] = string.c_str()[i];
+		}
+	}
+    return *this;
 };
 
 
 
 MyString MyString::operator+(char c){
-	;
-}; */
+	length_ = MyString::length();
+	if (length_ +1 < MyString::max_size()){
+		char* c_ = new char[length_+2];
+		for (unsigned i=0; i < length_ +2; i++){
+		   	c_[i] = string_[i];
+		}
+			c_[length_] = c;
+			c_[length_+1] = '\0';
+			MyString My_New_String (c_);
+			return My_New_String;
+	}
+	MyString My_New_String (string_);
+	return My_New_String;
+}; 
 
 
 
