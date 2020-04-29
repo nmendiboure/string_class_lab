0df86a55829f5ab3905da246381bcf7f18d0c2e8
Charles Nodot <charles.nodot@insa-lyon.fr>
2020-04-29 16:42:54 +0200

    "Ajout de tests pour les fonctions length() , maxsize(), size() et resize()"

--- MyString_test.cpp
+++ MyString_test.cpp
@@ -2,18 +2,8 @@
 #include "MyString.cpp"
 #include <iostream>
 TEST(GTestTests, DefautlConstructorTest) {
-	MyString string;
-	/*bool check=1;
-	char test[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r','l', 'd','\0'};
-	char stringtest[]= string.c_str(); 
-	for(int i=0; i<string.length();i++){
-		if(stringtest[i]!=test[i]){
-		check=0;
-		};
-	};
-	EXPECT_TRUE(check);
-	*/
-	const char* Cstring = string.c_str();
+	MyString def;
+	const char* Cstring = def.c_str();
 	EXPECT_STREQ(Cstring, "Hello World");
 };
 
@@ -28,60 +18,101 @@ TEST(GTestTests, CopyConstructorTest) {
 };
 
 TEST(GTestTests, CStrTest) {
-	MyString string("Hello World");
-	EXPECT_STREQ(string.c_str(), "Hello World");
+	MyString cStr("Hello World");
+	EXPECT_STREQ(cStr.c_str(), "Hello World");
 };
 
+TEST(GTestTests, SizeTest) {
+	MyString sizeT("Hello World");
+	EXPECT_EQ(sizeT.size(), 11);
+};
+
+
 TEST(GTestTests, ClearTest) {
-	MyString string("Hello World");
-	string.clear();
-	EXPECT_STREQ(string.c_str(), "");
+	MyString clearedStr("Hello World");
+	clearedStr.clear();
+	EXPECT_STREQ(clearedStr.c_str(), "");
 	size_t expected_size = 0;
-	EXPECT_EQ(string.size(), expected_size);
+	EXPECT_EQ(clearedStr.size(), expected_size);
 };
 
 
 TEST(GTestTests, CharAssignementOperatorTest) {
-	MyString string;
-	string='c';
-	EXPECT_STREQ(string.c_str(), "c");
+	MyString assignedStr;
+	assignedStr='c';
+	EXPECT_STREQ(assignedStr.c_str(), "c");
+	assignedStr='\0';
+	EXPECT_STREQ(assignedStr.c_str(), "");
+};
+
+
+TEST(GTestTests, StringConcatenationOperatorTest) {
+	MyString conStr1("AZERTYUIOP");
+	char* addedStr=new char[100000];
+	for(int i=0;i<100000;i++){
+		addedStr[i]='a';
+	}
+	addedStr[100000]='\0';
+	MyString test=conStr1+addedStr;
+	
+	EXPECT_STREQ(test.c_str(), "AZERTYUIOP");
+
+	char* addedStr2=new char[5];
+	for(int i=0;i<5;i++){
+		addedStr2[i]='a';
+	}
+	addedStr2[5]='\0';
+	MyString test2=conStr1+addedStr2;
+	EXPECT_STREQ(test2.c_str(), "AZERTYUIOPaaaaa");
+
+};
+
+TEST(GTestTests, ConstructorStringTest) {
+	MyString cStr("AZERTYUIOP");
+	EXPECT_STREQ(cStr.c_str(), "AZERTYUIOP");
+	MyString leer("");
+	EXPECT_STREQ(leer.c_str(), "");
+};
+
+TEST(GTestTests, MaxSizeStringTest) {
+	MyString lambda;
+	EXPECT_EQ(lambda.max_size(), 100);
+};
+
+TEST(GTestTests, LengthStringTest) {
+	MyString alphabet("abcdefghijklmnopqrstuvwxyz");
+	EXPECT_EQ(alphabet.length(), 26);
+	MyString empty("");
+	EXPECT_EQ(empty.length(), 0);
+};
+
+TEST(GTestTests, ResizeStringTest) {
+	MyString str1("azertyuiopqsdfghjklmwxcvbn");
+	str1.resize(4,'c');
+	EXPECT_STREQ(str1.c_str(), "azer");
+
+	MyString str2("azertyuiop");
+	str2.resize(12,'q');
+	EXPECT_STREQ(str2.c_str(), "azertyuiopqq");	
+	
+	str2.resize(100000000,'w');
+	EXPECT_STREQ(str2.c_str(), "azertyuiopqq");
 };
 
 TEST(GTestTests, MyStringAssignementOperatorTest) {
-	MyString string;
-	MyString string2("AZERTYUIOP");
-	string=string2;
-	EXPECT_STREQ(string2.c_str(), string.c_str());
-	EXPECT_STREQ(string.c_str(), "AZERTYUIOP");
+	MyString assignStr1;
+	MyString assignStr2("AZERTYUIOP");
+	assignStr1=assignStr2;
+	EXPECT_STREQ(assignStr2.c_str(), assignStr1.c_str());
+	EXPECT_STREQ(assignStr1.c_str(), "AZERTYUIOP");
 };
 
 TEST(GTestTests, CharConcatenationOperatorTest) {
-	MyString string3("AZERTYUIOP");
-	string3=string3+'z';
-	EXPECT_STREQ(string3.c_str(), "AZERTYUIOPz");
-	MyString string4("AZERTYUIOP");
-	string4=string4+'\0';
-	EXPECT_STREQ(string4.c_str(), "AZERTYUIOP");
-};
-//test de la fonction à corriger
-TEST(GTestTests, StringConcatenationOperatorTest) {
-	MyString s("AZERTYUIOP");
-	char* test=new char[100000];
-	for(int i=0;i<100000;i++){
-		test[i]='a';
-	}
-	test[100000]='\0';
-	MyString v=s+test;
-	
-	EXPECT_STREQ(v.c_str(), "AZERTYUIOP");
-
-	char* test2=new char[5];
-	for(int i=0;i<5;i++){
-		test2[i]='a';
-	}
-	test[5]='\0';
-	MyString p=s+test2;
-	EXPECT_STREQ(p.c_str(), "AZERTYUIOPaaaaa");
-
+	MyString concStr1("AZERTYUIOP");
+	concStr1=concStr1+'z';
+	EXPECT_STREQ(concStr1.c_str(), "AZERTYUIOPz");
+	MyString concStr2("AZERTYUIOP");
+	concStr2=concStr2+'\0';
+	EXPECT_STREQ(concStr2.c_str(), "AZERTYUIOP");
 };
 
